467618b081d64c4a1685a03468dd2926e446f2cf
karanmaurya15 <karanmaurya9663@gmail.com>
2023-03-30 20:30:56 +0530

    [Karan] added dashboard page

--- HospitalManagement/src/app/app-routing.module.ts
+++ HospitalManagement/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
 import { LoginComponent } from './Components/login/login.component';
 import { SignupComponent } from './Components/signup/signup.component';
 
 const routes: Routes = [
   {path:'signup',component:SignupComponent},
-  {path:'login',component:LoginComponent}
+  {path:'login',component:LoginComponent},
+  {path:'dashboard',component:DashboardComponent}
 ];
 
 @NgModule({
--- HospitalManagement/src/app/app.module.ts
+++ HospitalManagement/src/app/app.module.ts
@@ -6,21 +6,29 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SignupComponent } from './Components/signup/signup.component';
 import { LoginComponent } from './Components/login/login.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { DoctorsComponent } from './Components/doctors/doctors.component';
 
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     SignupComponent,
-    LoginComponent
+    LoginComponent,
+    DashboardComponent,
+    DoctorsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
+    AppRoutingModule,MatToolbarModule,MatCardModule,
     BrowserAnimationsModule,MatInputModule,MatFormFieldModule,MatIconModule,MatSlideToggleModule
   ],
   providers: [],
